54ec413bb93cbc869c76b80c26c53780add4c4b9
sana9407 <82316553+sana9407@users.noreply.github.com>
2021-04-11 17:38:56 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,481 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KEYWORDSMAPPING</title>
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    
+
+</head>
+<body>
+	<header>
+		<div class="menu-toggler">
+			<div class="bar half start"></div>
+			<div class="bar"></div>
+			<div class="bar half end"></div>
+		</div>
+		<nav class="top-nav">
+			<ul class="nav-list">
+				<li>
+					<a href="#about" class="nav-link">ABOUT</a>
+				</li>
+				<li>
+					<a href="#experience" class="nav-link">HOW IT WORKS?</a>
+				</li>
+				<li>
+					<a href="#services" class="nav-link">OUR PACKAGES & PRICING</a>
+				</li>
+				<li>
+					<a href="#testimonials" class="nav-link">TESTIMONIALS</a>
+				</li>
+				<li>
+					<a href="#contact" class="nav-link">ENQUIRY</a>
+				</li>
+			</ul>
+		</nav>
+
+		<div class="landing-text">
+		<div class="">	
+
+			<h1>KEYWORDS<br>MAPPING</h1>
+		
+			<h6 >Boost Performance of your CEO & PPC Campaign</h6>
+			<h6 >A Successful Formula in Digital Marketing</h6><br></div><br><br><br><br>
+			<h5 >14,688 CAMPAIGNS COMPLETED<br>3,572 HAPPY BUSINESSES</h5>
+		</div>
+		
+
+	</header>
+
+	<section class="about" id="about">
+		<div class="container">
+			<div class="profile-img " data-aos="fade-right" data-aos-delay="300">
+				<img src="images/profile.jpeg" alt="">
+			</div>
+			<div class="about-details" data-aos="fade-left" data-aos-delay="600">
+				<div class="about-heading">
+					<h1>ABOUT</h1>
+					<h6>KEYWORDS MAPPING</h6>
+				</div>
+				<h5 class="h5">WANT RELEVANT WEBSITE TRAFFICE?</h5><br>
+			
+				<h6 class="about-h6"> Looking for maximum Business coversion? &nbsp; &nbsp;<a href="#services" class="btn">Try Now</a></h6><br>
+				<h6 class="about-h66">We make sure, your online audiencess are visiting to the right laning page</h6><br>
+				<h5 class="about-h55"> KEYWORDS MAPPING IS A TEDIOUS AND TIME CONSUMING JOB, BUT WE LOVE TOO DO THIS FOR YOU &nbsp; &nbsp;<a href="#services" class="btn">Start Now</a></h5><br>
+				<h5 class="h555">Company Profile</h5><br>
+				<h6 class="h666">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;We are Digital Marketing expert with having a more than 13 years of Internet Marketing experience. Our strong Digital Marketing professionals are helping to fulfill your online marketing, advertising and promotional need.<br>We served '<b>3,572</b>' local Businesses around '<b>USA</b>','<b>UK</b>','<b>CAN</b>'and'<b>AUS</b>', which includes - 'Auto Dealers, Rooters, Roofing Contractor, Plumbers, Electrician, Handyman Services, Kitchen & Bath Designs, Landscaping, Auto Service & Repairs, Auto Body Repair & Collision, Auto Insurance, Junk & Waste Removal, Hotels and Motels, Tours & Travels, Tree Care & Removal Services, Universities, Moving & Storage, Personal Injury Lawyer, DUI/DWI Attorney, Family & Divorce Attorney, Cosmetic & Plastic Surgery, Dentist, Chiropractor, Independent-Assisted Living, Hospice Care, Real State Agents' etc.</h6> 
+
+
+				<div class="social-media">
+				<ul class="nav-list">
+					<li>
+						<a href="#" class="icon-link">
+							<i class="fab fa-facebook-square"></i>
+						</a>
+					</li>
+					<li>
+						<a href="#" class="icon-link">
+							<i class="fab fa-twitter-square"></i>
+						</a>
+					</li>
+					<li>
+						<a href="#" class="icon-link">
+							<i class="fab fa-dribbble-square"></i>
+						</a>
+					</li>
+				</ul>
+			</div>
+			</div>
+		</div>
+	</section>
+
+
+	<section class="experience" id="experience"> 
+		<div class="container">
+			<div class="section-heading">
+				<h1>How It Works?</h1>
+				
+			</div>
+			<div class="timeline" data-aos="fade-down" data-aos-delay="300">
+				<ul>
+					<li class="date" data-date="STEP- 1">
+					
+					<p>
+					SELECT PACKAGE AND PRICING
+					</p>
+					</li>
+
+					<li class="date" data-date="STEP- 2">
+						
+						<p>
+						UPLOAD KEYWORD LIST
+					</p>
+					</li>
+
+					<li class="date" data-date="STEP- 3">
+						
+						<p>
+						MAKE A PAYMENT
+					</p>
+					</li>
+					<li class="date" data-date="STEP- 4">
+						
+						<p>
+						RECEIVE A SPREADSHEET OF MAPPED KEYWORDS AFTER 24 HOUR's
+					</p>
+					</li
+				</ul>
+			</div>
+		</div>
+	</section>
+
+	<section class="services" id="services"> 
+		<div class="container">
+			<div class="section-heading">
+				<h1>Our Packages & Pricing</h1>
+				
+			</div>
+			
+			<div class="my-skills">
+				<div class="skill" data-aos="fade-right" data-aos-delay="300">
+					<div class="icon-container">
+						<i class='no-italics'> TRIAL </i>
+					</div>
+				 	<h1>$50/Per Campaign</h1>
+					<ul class="list">
+						<li>
+							UPTO 500 KEYWORDS.
+						</li>
+						<li>
+							10 LANDING PAGES INSERTION.
+						</li>
+						<li>
+							SERVICES KEYWORD MAPPING.
+						</li>
+						<li>
+							PRODUCTION KEYWWORD MAPPING.
+						</li>
+						<li>
+							GENERIC KEYWORDS MAPPING
+						</li>
+						<li>
+							24 HOURS TURNAROUND TIME.
+						</li>
+					</ul><br><br><br>
+					<div>
+
+					<p class="textupload">Upload your keywords here</p><br><hr>
+
+
+
+					<form action="uploadfile.php" method="POST" enctype="multipart/form-data">
+ 	<div>
+  <input type="file" id="file" name="file" class="uploadfile" > <input type="submit" name="btn" ><hr></div>
+</form>
+
+					<div >
+						<a href="https://www.google.com/" class="button">Order Now</a>
+					</div>
+				</div>
+			</div>
+				
+
+
+				<div class="skill" data-aos="fade-left" data-aos-delay="600">
+					<div class="icon-container">
+						<i class='no-italics'> INDIVIDUAL </i>
+					</div>
+					<h1>$300/month</h1>
+					<ul class="list">
+						<li>
+							UPTO 1000 KEYWORDS
+						</li>
+						<li>
+							30 LANDING PAGES INSERTION
+						</li>
+						<li>
+							SERVICE KEYWORDS MAPPING
+						</li>
+						<li>
+							PRODUCT KEYWORDS MAPPING
+						</li>
+						<li>
+							GENERIC KEYWORDS MAPPING
+						</li>
+						<li>
+							5 BRANDS KEYWORDS MAPPING
+						</li>
+						<li>
+							24 HOURS TURNAROUND TIME
+						</li>
+					</ul>
+					<div>
+
+					<p class="textupload">Upload your keywords here</p><br><hr>
+
+					<form action="uploadfile.php" method="POST" enctype="multipart/form-data">
+ 	<div>
+  <input type="file" id="file" name="file" multiple class="uploadfile" > <input type="submit" class="uploadbtn"><hr></div>
+</form>
+
+					<div >
+						<a href="https://www.google.com/" class="button">Order Now</a>
+					</div>
+				</div>
+				</div>
+			</div>
+			<div class="my-skills">
+				<div class="skill" data-aos="fade-right" data-aos-delay="300">
+					<div class="icon-container">
+						<i class='no-italics'> SMALL/MEDIUM BUSINESSES </i>
+					</div>
+				 	<h1>$600/month</h1>
+						<ul class="list">
+						<li>
+							UPTO 2000 KEYWORDS
+						</li>
+						<li>
+							50 LANDING PAGES INSERTION
+						</li>
+						<li>
+							SERVICE KEYWORDS MAPPING
+						</li>
+						<li>
+							PRODUCT KEYWORDS MAPPING
+						</li>
+						<li>
+							GENERIC KEYWORDS MAPPING
+						</li>
+						<li>
+							10 BRANDS KEYWORDS MAPPING
+						</li>
+						<li>
+							DYNAMIC LANDING PAGE UPDATION
+						</li>
+						<li>
+							24 HOURS TURNAROUND TIME
+						</li>
+					</ul>
+					<div>
+
+					<p class="textupload">Upload your keywords here</p><br><hr>
+
+					<form action="uploadfile.php" method="POST" enctype="multipart/form-data">
+ 	<div>
+  <input type="file" id="file" name="file" multiple class="uploadfile" > <input type="submit" class="uploadbtn"><hr></div>
+</form>
+
+					<div >
+						<a href="https://www.google.com/" class="button">Order Now</a>
+					</div>
+				</div>
+				</div>
+				<div class="skill" data-aos="fade-left" data-aos-delay="600">
+					<div class="icon-container">
+						<i class='no-italics'> AGENCY/E-COMMERCE </i>
+					</div>
+					<h1>$900/month</h1>
+						<ul class="list">
+						<li>
+							UPTO 3000 KEYWORDS.
+						</li>
+						<li>
+							100 LANDING PAGES INSERTION.
+						</li>
+						<li>
+							SERVICE KEYWORDS MAPPING.
+						</li>
+						<li>
+							PRODUCT KEYWORDS MAPPING.
+						</li>
+						<li>
+							GENERIC KEYWORDS MAPPING.
+						</li>
+						<li>
+							15 BRANDS KEYWORDS MAPPING.
+						</li>
+						<li>
+							DYNAMIC LANDING PAGE UPDATION.
+						</li>
+						<li>
+							24 HOURS TURNAROUND TIME.
+						</li>
+					</ul>
+					<div>
+
+					<p class="textupload">Upload your keywords here</p><br><hr>
+
+					<form action="uploadfile.php" method="POST" enctype="multipart/form-data">
+ 	<div>
+  <input type="file" id="file" name="file" multiple class="uploadfile" > <input type="submit" class="uploadbtn"><hr></div>
+</form>
+
+					<div >
+						<a href="https://www.google.com/" class="button">Order Now</a>
+					</div>
+				</div>
+				</div>
+				
+			</div>
+			<div class="skill" >
+					<h2 class="more">DO YOU HAVE MORE <br> KEYWORDS FOR MAPPING?</h2>
+					<h3 class="more1">Ask For Custom Package</h3><br><br>
+					<div >
+						<a href="#contact" class="button">Enquire Now</a>
+					</div>
+				</div>
+
+			
+		
+		</div>
+	</section>
+
+
+		<section class="testimonials" id="testimonials"> 
+		<div class="container">
+			<div class="section-heading">
+				<h1>Testimonials</h1>
+				
+			</div>
+			<!-- Slideshow container -->
+<div class="slideshow-container">
+
+  <!-- Full-width images with number and caption text -->
+  <div class="mySlides fade">
+    <div class="numbertext">1 / 9</div>
+    <img src="images/white.jpg" class="Testimonial-img">
+    <div class="text12">Keywords Mappings team really do a wonderful job in terms of Dynamic Keywords Insertion.</div>
+    <div class="text">Charles M. - Tree Removal Services, CA</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">2 / 3</div>
+    <img src="images/white.jpg" class="Testimonial-img">
+    <div class="text12">We have a small Business of carpet cleaning in SF. For me, Keywords Mapping worked effectively for my SEO.</div>
+    <div class="text">Franko - Carpet Cleaning Services, SF</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">3 / 3</div>
+    <img src="images/white.jpg" class="Testimonial-img" >
+    <div class="text12">Previously, i spent over $9000 into PPC Ads, but didn't received relevant business enquiries. Thanks to DKI techniques of Keywords Mapping, now i am getting good business everyday.</div>
+    <div class="text">Joel Weaver - Handyman, CA</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">3 / 3</div>
+    <img src="images/white.jpg" class="Testimonial-img" >
+    <div class="text12">DKI Service helped to boost my business identity online. My web presence spread over the entire NY. It was really helpful to create a brand awareness.</div>
+    <div class="text">John Lam - Locksmith, New Jersy</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">3 / 3</div>
+    <img src="images/white.jpg" class="Testimonial-img" >
+    <div class="text12">I would highly recommend to get best quality Keywords Mapping done which is good for any business that wants more online exposure.</div>
+    <div class="text">Christopher - Auto Body Shop & <br>Collision Centre, Forest Hill Queens NY</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">3 / 3</div>
+    <img src="images/white.jpg" class="Testimonial-img" >
+    <div class="text12">Accurate Keywords Mapping into DLPs are helping me to receive a good amount of website traffic. Thanks.</div>
+    <div class="text">Florene H - Dentist, Bay Area CA</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">3 / 3</div>
+    <img src="images/white.jpg" class="Testimonial-img" >
+    <div class="text12">With the help of Keywords Mapping services we are getting more visitor's traffic to our showroom. Good work.</div>
+    <div class="text">Robert Cosa - Auto Dealer, MO</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">3 / 3</div>
+    <img src="images/white.jpg" class="Testimonial-img" >
+    <div class="text12">They have a professional team with a great support. You can expect the best quality work from them.</div>
+    <div class="text">Kathrine - Personal Injury Lawyer, Chicago</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">3 / 3</div>
+    <img src="images/white.jpg" class="Testimonial-img" >
+    <div class="text12">Relevant Business enquiries we are receiving for our removal services throughout NSW. Thanks.</div>
+    <div class="text">Michael - Storage & Removal services, <br>Sydney AUS</div>
+  </div>
+
+  <!-- Next and previous buttons -->
+  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<br>
+
+<!-- The dots/circles -->
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span>
+  <span class="dot" onclick="currentSlide(2)"></span>
+  <span class="dot" onclick="currentSlide(3)"></span>
+  <span class="dot" onclick="currentSlide(4)"></span>
+  <span class="dot" onclick="currentSlide(5)"></span>
+  <span class="dot" onclick="currentSlide(6)"></span>
+  <span class="dot" onclick="currentSlide(7)"></span>
+  <span class="dot" onclick="currentSlide(8)"></span>
+  <span class="dot" onclick="currentSlide(9)"></span>
+</div>
+
+
+	  </div>
+</section>
+	
+
+
+	
+
+	<section class="contact" id="contact"> 
+		<div class="container">
+			<div class="section-heading">
+				<h1>ENQUIRY</h1>
+				
+			</div>
+			<form action="index.php" method="post" data-aos="fade-up" data-aos-delay="300" class="form">
+				<label for="name">Name:</label><br>
+				<input type="text" id="name" name="name" placeholder="Enter your name..." required>
+				<label for="email">Email:</label><br>
+				<input type="email" id="email" name="email" placeholder="Enter your Email..." required>
+				  <label for="phone">Contact Number:</label>
+  <input type="tel" id="phone" name="phone" placeholder="Enter your contact number"  required>
+				<label>enquiry:</label>
+				<textarea name="subject" id="subject" class="form-control" cols="10" rows="8"></textarea><br>
+				<input type="submit" name="submit" >
+				
+
+   
+
+			</form>
+		</div>
+	</section>
+
+
+
+
+
+
+		</div>
+	</section>
+
+	<footer class="copyright">
+		
+		<p>&copy: 2020 KEYWORDSMAPPING</p>
+	</footer>
+
+		
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script src="main.js"></script>
+    <script src="script.js"></script>
+</body>
+</html>
